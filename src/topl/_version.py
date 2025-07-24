bdcd42e076e1cd2b5655a340c70a6092105ee0b5
Adam Twardoch <adam+github@twardoch.com>
2025-07-24 14:02:27 +0200

    v1.0.4

--- src/topl/_version.py
+++ src/topl/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.3.dev0+g4d617ce.d20250724'
-__version_tuple__ = version_tuple = (1, 0, 3, 'dev0', 'g4d617ce.d20250724')
+__version__ = version = '1.0.4.dev0+g30534d0.d20250724'
+__version_tuple__ = version_tuple = (1, 0, 4, 'dev0', 'g30534d0.d20250724')
